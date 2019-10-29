4e68733b1a484031278d267b199bef5d98a25f01
inoublii <inoublii@gmail.com>
2019-10-29 11:19:25 +0000

    the begining

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "holberton.h"
+/**
+* _printf - function to print anything
+* @format: given argument
+* Return: int
+*/
+int_printf(const char *format, ...)
+{
+	
+}
