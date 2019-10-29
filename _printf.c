97708bcb310c46789c80321123276d571e63c8ee
YosriGFX <1111@holbertonschool.com>
2019-10-29 12:48:16 +0000

    first try on printf

--- _printf.c
+++ _printf.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdarg.h>
 #include "holberton.h"
 /**
@@ -8,5 +7,27 @@
 */
 int_printf(const char *format, ...)
 {
-	
+unsigned int a = 0, b;
+char *s;
+va_list all;
+va_start(all, format);
+while(format[a] != '\0')
+{
+if (format[a] == '%')
+{
+a++;
+if (format[a] == 's' || format[a] == 'c')
+{
+s = va_arg(all, char*);
+b = 0;
+while (s[b] != '\0')
+{
+_putchar(s[b]);
+b++;
+}
+return (b);
+}
+}
+a++;
+}	
 }
